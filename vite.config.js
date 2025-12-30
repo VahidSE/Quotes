d1a27264cdd9a2d1a7a2f217c00f537f7bf3723c
Vahid <vahid>
2025-12-30 20:36:52 +0530

    modified base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Quotes/',
+  base: '/quotes-app/',
   plugins: [
     react({
       babel: {
